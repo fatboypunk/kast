4d6f7080e06459e8f739ceae4984bf408fd09587
FatboyPunk <marcel.horlings@gmail.com>
2019-01-25 21:01:24 +0100

    add some debugging

--- screen_on_off.py
+++ screen_on_off.py
@@ -1,18 +1,25 @@
 import RPi.GPIO as GPIO
-from time import sleep     # this lets us have a time delay (see line 15)
-GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
-GPIO.setup(17, GPIO.IN)    # set GPIO17 as input (button)
+from time import sleep
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.IN)
 from subprocess import call
-from datetime import datetime, timedelta
 
-TEN_MINUTES = 60000
+TEN_MINUTES = 600000
+ONE_MINUTE = 6
 
 try:
   while True:
-    channel = GPIO.wait_for_edge(17, GPIO.RISING, timeout=TEN_MINUTES)
+    print('waiting')
+    channel = GPIO.wait_for_edge(17, GPIO.RISING, timeout=    TEN_MINUTES)
     if channel is None:
+      print('been quite for 10 min')
       call(["/usr/bin/vcgencmd", "display_power", "0"])
     else:
+      print('heard a sound')
+      print(dir(channel))
       call(["/usr/bin/vcgencmd", "display_power", "1"])
-finally:                   # this block will run no matter how the try block exits
-  GPIO.cleanup()         # clean up after yourself
+      sleep(ONE_MINUTE)
+      print('stopped_sleeping')
+
+finally:▫
+  GPIO.cleanup()▫
