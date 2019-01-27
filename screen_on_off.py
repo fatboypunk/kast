a22c5e5cda1dadcbbe5c0e464beb866f545cf1bb
FatboyPunk <marcel.horlings@gmail.com>
2019-01-27 16:02:47 +0100

    add a daemon to the code

--- screen_on_off.py
+++ screen_on_off.py
@@ -3,23 +3,36 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN)
 from subprocess import call
+import daemon
+import lockfile
+import signal
 
 TEN_MINUTES = 600000
-ONE_MINUTE = 6
+ONE_MINUTE = 60
 
-try:
-  while True:
-    print('waiting')
-    channel = GPIO.wait_for_edge(27, GPIO.RISING, timeout=TEN_MINUTES)
-    if channel is None:
-      print('been quite for 10 min')
-      call(["/usr/bin/vcgencmd", "display_power", "0"])
-    else:
-      print('heard a sound')
-      print(dir(channel))
-      call(["/usr/bin/vcgencmd", "display_power", "1"])
-      sleep(ONE_MINUTE)
-      print('stopped_sleeping')
-
-finally:
+def shutdown(signum, frame):  # signum and frame are mandatory
   GPIO.cleanup()
+  sys.exit(0)
+
+def main():
+  try:
+    while True:
+      channel = GPIO.wait_for_edge(27, GPIO.RISING, timeout=TEN_MINUTES)
+      if channel is None:
+        call(["/usr/bin/vcgencmd", "display_power", "0"])
+      else:
+        call(["/usr/bin/vcgencmd", "display_power", "1"])
+        sleep(ONE_MINUTE)
+
+  finally:
+    shutdown('', '')
+
+with daemon.DaemonContext(
+  pidfile=lockfile.FileLock('/tmp/screen_on_off.pid'),
+  signal_map={
+    signal.SIGINT: shutdown,
+    signal.SIGKILL: shutdown,
+    signal.SIGTERM: shutdown,
+    signal.SIGTSTP: shutdown
+  }):
+  main()
