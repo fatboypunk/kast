e39536cd1461108676725345dd65a33f177e26ed
FatboyPunk <marcel.horlings@gmail.com>
2019-02-03 19:37:44 +0100

    test logging without gpio

--- screen_on_off.py
+++ screen_on_off.py
@@ -1,17 +1,17 @@
 #! /usr/bin/python
 # -*- coding: utf-8; -*-
-import RPi.GPIO as GPIO
+
+# import RPi.GPIO as GPIO
 from time import sleep
 from subprocess import call
 import logging
 import daemon
-import daemon.pidlockfile
 import sys
 import lockfile
 import signal
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.IN)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(27, GPIO.IN)
 
 logger = logging.getLogger("DaemonLog")
 logger.setLevel(logging.INFO)
@@ -22,32 +22,38 @@ logger.addHandler(handler)
 TEN_MINUTES = 600000
 ONE_MINUTE = 60
 
-def shutdown(signum, frame):
-  logger.info('Shutting down, bye bye')
-  GPIO.cleanup()
-  sys.exit(0)
+# def shutdown(signum, frame):
+#   logger.info('Shutting down, bye bye')
+#   GPIO.cleanup()
+#   sys.exit(0)
+
+# def main():
+#   try:
+#     while True:
+#       logger.info('Starting')
+#       channel = GPIO.wait_for_edge(27, GPIO.RISING, timeout=TEN_MINUTES)
+#       if channel is None:
+#         logger.info('Not heard anything for 10 minutes, power down')
+#         call(["/usr/bin/vcgencmd", "display_power", "0"])
+#       else:
+#         logger.info('Heard something, power on')
+#         call(["/usr/bin/vcgencmd", "display_power", "1"])
+#         logger.info('Sleeping')
+#         sleep(ONE_MINUTE)
+#         logger.info('Sleeping is over')
+
+#   finally:
+#     shutdown('', '')
 
 def main():
   try:
-    while True:
-      logger.info('Starting')
-      channel = GPIO.wait_for_edge(27, GPIO.RISING, timeout=TEN_MINUTES)
-      if channel is None:
-        logger.info('Not heard anything for 10 minutes, power down')
-        call(["/usr/bin/vcgencmd", "display_power", "0"])
-      else:
-        logger.info('Heard something, power on')
-        call(["/usr/bin/vcgencmd", "display_power", "1"])
-        logger.info('Sleeping')
-        sleep(ONE_MINUTE)
-        logger.info('Sleeping is over')
+    logger.info('log this')
 
   finally:
-    shutdown('', '')
+    logger.info('the end')
 
 with daemon.DaemonContext(
-        pidfile=lockfile.FileLock('/var/run/screen_on_off.pid'),
         files_preserve=[handler.stream],
-        working_directory='/home/pi/kast'):
+        working_directory='/Users/marcel/code/kast'):
   logger.info('Starting as a daemon')
   main()
